f83fd78fd5bc20ad055080e7be65cf86337707e9
mihengels <m1hengels@yandex.ru>
2025-10-26 04:13:28 +0300

    First Hello Driver made

---
+++ HelloDriver/hello.c
@@ -0,0 +1,34 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+
+MODULE_LICENSE("Dual BSD/GPL");
+//AUTOR("MIHENGELS");
+
+static char *whom = "world";
+static int howmany;
+
+module_param(howmany, int, S_IRUGO);
+module_param(whom, charp, S_IRUGO);
+
+static int __init hello_init(void)
+{
+    for (int i = 0; i < howmany; ++i) { 
+        printk(KERN_ALERT "Hello, %s\n", whom);
+    }
+    return 0;
+}
+
+static void hello_exit(void)
+{
+    printk(KERN_ALERT "Goodbye, world\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+
+
+
+
+
